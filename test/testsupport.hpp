53010c1b12cc3c1276fc8817bc728c8d543d1f80
Paul Dreik <github@pauldreik.se>
2018-10-04 16:09:47 +0200

    add missing licenses

--- test/testsupport.hpp
+++ test/testsupport.hpp
@@ -1,4 +1,10 @@
-
+/*
+ * By Paul Dreik 2018
+ *
+ * License:
+ * dual license, pick your choice. Either Boost license 1.0, or GPL(v2 or later,
+ * at your option).
+ */
 #pragma once
 
 #include <array>
